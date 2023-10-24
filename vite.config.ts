b6a0678409a501d9257b739168027936439cc627
Liaksej <tarusov.liaksej@gmail.com>
2023-10-24 15:32:41 +0200

    Update base URL and Rollup options in vite.config.ts
    The vite.config.ts file has been updated with new configurations. The base URL has been changed from './' to '/ra-react-router-menu/' and the Rollup options have been simplified by enabling sourcemaps. These adjustments are meant to streamline the deployment process and enhance routing in the deployed environment. Additionally, a 'gh-pages' dependency has been installed due to changes made in root reference.

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: "/absolute/path/to/main.js",
-    },
-  },
   plugins: [react()],
+  base: "/ra-react-router-menu/",
+  build: {
+    sourcemap: true,
+  },
 });
